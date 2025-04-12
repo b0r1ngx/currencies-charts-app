af355960d162bfd6242d74944db6c61a8a8d62b2
Nikita <smirnov2.nv@gmail.com>
2025-04-12 18:02:50 +0300

    deleted empty class

--- app/src/main/java/com/boringxcompany/charts/currency/ui/BottomNavBar.kt
+++
@@ -1,9 +0,0 @@
-package com.boringxcompany.charts.currency.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun BottomNavBar() {
-    // main icon
-    // profile icon
-}
--- app/src/main/java/com/boringxcompany/charts/currency/ui/screen/UserScreen.kt
+++ app/src/main/java/com/boringxcompany/charts/currency/ui/screen/UserScreen.kt
@@ -2,7 +2,6 @@ package com.boringxcompany.charts.currency.ui.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.boringxcompany.charts.currency.data.Screen
 
 @Composable
 fun UserScreen() {
