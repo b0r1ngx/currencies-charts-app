0d407aa610e11bb3646ee83b5f467f9c3f6e5170
Ivanov Kirill A <perkeboring@gmail.com>
2025-04-13 13:48:40 +0300

    partly implement HomeViewModel

---
+++ app/src/main/java/com/boringxcompany/charts/currency/viewmodel/HomeViewModel.kt
@@ -0,0 +1,34 @@
+package com.boringxcompany.charts.currency.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.boringxcompany.charts.currency.data.domain.Currency
+import com.boringxcompany.charts.currency.repository.currency.CurrenciesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val currenciesRepository: CurrenciesRepository,
+) : ViewModel() {
+    private val _currencies: MutableStateFlow<List<Currency>> = MutableStateFlow(listOf())
+    val currencies: SharedFlow<List<Currency>> = _currencies.asStateFlow()
+
+    init {
+        collectCurrencies()
+    }
+
+    fun collectCurrencies() {
+        viewModelScope.launch {
+            try {
+                val currencies = currenciesRepository.getCurrencies()
+//                todo: _currencies.update({ currencies })
+            } catch (e: Exception) {
+                // todo
+            }
+        }
+    }
+
+}
