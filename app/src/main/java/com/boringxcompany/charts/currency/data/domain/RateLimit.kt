ab91009c7c055fc5b481cf4ecd10f301fb0c4af3
Nikita <smirnov2.nv@gmail.com>
2025-04-17 13:34:39 +0300

    Deleted unused files

--- app/src/main/java/com/boringxcompany/charts/currency/data/domain/RateLimit.kt
+++
@@ -1,6 +0,0 @@
-package com.boringxcompany.charts.currency.data.domain
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class RateLimit
--- app/src/main/java/com/boringxcompany/charts/currency/data/domain/Rating.kt
+++
@@ -1,8 +0,0 @@
-package com.boringxcompany.charts.currency.data.domain
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Rating(
-    val Weiss: Weiss
-)
