fe545be9aefa65cf01a41c659fe518d2f30ff200
Nikita <smirnov2.nv@gmail.com>
2025-04-17 13:38:21 +0300

    Changed the name of Currency to a more appropriate one

---
+++ app/src/main/java/com/boringxcompany/charts/currency/data/domain/GeneralCoinInfo.kt
@@ -0,0 +1,11 @@
+package com.boringxcompany.charts.currency.data.domain
+
+data class GeneralCoinInfo(
+    val image: String?,
+    val fullName: String?,
+    val name: String?,
+    val price: String?,
+    val dailyPriceChangePercent: String?,
+    val dailyVolume: String?,
+    val marketCap: String?,
+)
