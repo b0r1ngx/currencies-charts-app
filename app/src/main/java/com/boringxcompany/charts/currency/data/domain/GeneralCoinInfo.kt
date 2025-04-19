23b19ede902e13e4de2d2d4eb22e229e3bfc2750
Ivanov Kirill A <perkeboring@gmail.com>
2025-04-19 18:58:57 +0300

    GeneralCoinInfo: add index to show on UI position of currency, rename dailyVolume to dailyTradeVolume

--- app/src/main/java/com/boringxcompany/charts/currency/data/domain/GeneralCoinInfo.kt
+++ app/src/main/java/com/boringxcompany/charts/currency/data/domain/GeneralCoinInfo.kt
@@ -1,11 +1,12 @@
 package com.boringxcompany.charts.currency.data.domain
 
 data class GeneralCoinInfo(
+    val index: Int,
     val image: String?,
     val fullName: String?,
     val name: String?,
     val price: String?,
     val dailyPriceChangePercent: String?,
-    val dailyVolume: String?,
+    val dailyTradeVolume: String?,
     val marketCap: String?,
 )
