b6068d9bacd9643ccaa298db4d5ed4179ff1370d
Ivanov Kirill A <perkeboring@gmail.com>
2025-06-14 20:12:35 +0300

    remove app module

--- app/src/main/java/com/boringxcompany/charts/currency/repository/currency/determineBestCurrencyApiForUser.kt
+++
@@ -1,9 +0,0 @@
-package com.boringxcompany.charts.currency.repository.currency
-
-import com.boringxcompany.charts.currency.api.CoinDeskApi
-import com.boringxcompany.charts.currency.api.CurrencyApi
-import com.boringxcompany.charts.currency.client.KtorClient
-
-fun determineBestCurrencyApiForUser(): CurrencyApi {
-    return CoinDeskApi(KtorClient())
-}
--- app/src/main/java/com/boringxcompany/charts/currency/repository/user/UserRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.boringxcompany.charts.currency.repository.user
-
-interface UserRepository {
-
-}
