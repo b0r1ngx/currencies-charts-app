89a7760e08285ba5d25d9c2430ed53bc735ba357
Ivanov Kirill A <perkeboring@gmail.com>
2025-09-20 23:10:19 +0300

    constants moved to separate file, bc it may be reused in different API's (and ofc all usages of data source API's must be behind self-developed backend, minimum for two principles: 1) not expose private keys; 2) decrease amount calls to data source API's)

---
+++ client/src/commonMain/kotlin/api/constants.kt
@@ -0,0 +1,5 @@
+package api
+
+const val AMOUNT_OF_CURRENCIES = 100
+// TODO: Allow to set this in user preferences (right on home screen, to be more visible / easy to change, or move to ProfileScreen?)
+const val QUOTE_CURRENCY = "USD"
